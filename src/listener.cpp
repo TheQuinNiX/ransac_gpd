c1541b5e8a038c185022113b04c763a8ce1c9572
TheQuinNiX <jonaswiese@gmx.net>
2023-07-05 16:54:19 +0000

    Fixed some bugs. FindCylinder is finally working.

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,24 +1,19 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
-#include "pcl_ros/point_cloud.h"
-#include "pcl/point_types.h"
-#include "pcl_ros/transforms.h"
-#include "pcl/sample_consensus/ransac.h"
-#include "pcl/sample_consensus/sac_model_plane.h"
-#include "pcl/segmentation/sac_segmentation.h"
-#include "pcl/filters/extract_indices.h"
-#include "pcl/filters/passthrough.h"
 #include "visualization_msgs/Marker.h"
-#include "pcl/features/normal_3d.h"
+#include "pcl/point_types.h"
+#include "tf2_eigen/tf2_eigen.h"
+#include "pcl_ros/point_cloud.h"
+#include "pcl/point_cloud.h"
+#include "pcl/segmentation/sac_segmentation.h"
+#include "pcl/features/normal_3d_omp.h"
+#include "pcl_ros/transforms.h"
+#include "pcl/filters/passthrough.h"
 #include "time.h"
-#include "limits"
-#include "iostream"
-#include "math.h"
-#include "float.h"
+#include "pcl/filters/extract_indices.h"
 
 ros::Publisher pub_pointcloud;
 ros::Publisher pub_marker;
-typedef pcl::PointXYZ PointT;
 
 // This function checks a point for NaN value. If Point value is NaN the function returns 1, otherwise 0.
 bool checkPointNaN(pcl::PointXYZ& input)
@@ -28,6 +23,20 @@ bool checkPointNaN(pcl::PointXYZ& input)
         return false;
     }
     return true;
+}
+
+// Calculates quaternion from RANSAC model cofficients.
+inline Eigen::Quaternionf obtainCylinderOrientationFromModel(pcl::ModelCoefficients& coefficients)
+{
+    Eigen::Vector3f axis_vector(coefficients.values.at(3), coefficients.values.at(4), coefficients.values.at(5));
+    Eigen::Vector3f up_vector(0.0, 0.0, -1.0);
+    Eigen::Vector3f right_vector = axis_vector.cross(up_vector);
+    right_vector.normalized();
+
+    Eigen::Quaternionf q(Eigen::AngleAxisf(-1.0 * std::acos(axis_vector.dot(up_vector)), right_vector));
+    q.normalize();
+
+    return q;
 }
 
 // This function returns the point with the highest z value from a pointcloud.
@@ -99,7 +108,7 @@ pcl::PointXYZ getPointMaxZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input, pcl::Poin
     return point_z_max;
 }
 
-// This function returns the point with the lowest z value from a pointcloud.
+// Returns point with lowest z value from a pointcloud.
 pcl::PointXYZ getPointMinZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
 {
     pcl::PointXYZ point_z_min;
@@ -116,7 +125,7 @@ pcl::PointXYZ getPointMinZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
             {            
                 point_temp = input->at(c, r);
 
-                if (!checkPointNaN(point_temp))
+                if (!checkPointNaN(point_temp) or (point_temp.z != 0.0))
                 {                
                     if (point_z_min.z > point_temp.z)
                     {
@@ -134,7 +143,7 @@ pcl::PointXYZ getPointMinZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
         {            
             point_temp = input->at(i);
 
-            if (!checkPointNaN(point_temp))
+            if (!checkPointNaN(point_temp) or (point_temp.z != 0.0))
             {                
                 if (point_z_min.z > point_temp.z)
                 {
@@ -147,7 +156,7 @@ pcl::PointXYZ getPointMinZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
     return point_z_min;
 }
 
-// This function adds the highest negative z value of the pointcloud, to each point of a pointcloud, so minimum z value will be 0.0.
+// Adds highest negative z value of the pointcloud, to each point of a pointcloud, so minimum z value will be 0.0.
 void moveUp(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
 {
     pcl::PointXYZ point_z_min = getPointMinZ(input);
@@ -182,6 +191,7 @@ void moveUp(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
     }
 }
 
+// This function subtracts the lowest z value of the pointcloud, from each point of a pointcloud, so minimum z value will be 0.0.
 void moveDown(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
 {
     pcl::PointXYZ point_z_min = getPointMinZ(input);
@@ -210,9 +220,6 @@ void moveDown(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
         {
             if (!checkPointNaN(input->at(i)))
             {
-                ROS_INFO_STREAM("at 0 = " << input->at(0).z);
-                ROS_INFO_STREAM("input z at i = " << input->at(i).z);
-                ROS_INFO_STREAM("pointcloud z min = " << fabsf(point_z_min.z));
                 input->at(i).z = input->at(i).z - fabsf(point_z_min.z);
                 
             }
@@ -220,7 +227,7 @@ void moveDown(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
     }
 }
 
-// This function mirrors the pointcloud in z direction.
+// Mirrors the pointcloud in z direction.
 void mirrorPointcloudZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
 {
     int input_length = input->size();
@@ -234,78 +241,7 @@ void mirrorPointcloudZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
     }
 }
 
-float getAngle(pcl::PointXYZ& a, pcl::PointXYZ& b)
-{
-    float ak;
-    float gk;
-    float r;
-
-    if (a.x == b.x)
-    {
-        // calculate ak
-        if (a.y < b.y)
-        {
-            ak = b.y - a.y;
-        }
-        else
-        {
-            ak = a.y - b.y;
-        }
-
-        // calculate gk
-        if (a.z < b.z)
-        {
-            gk = b.z - a.z;
-        }
-        else
-        {
-            gk = a.z - b.z;
-        }
-
-        //calculate tan
-        r = atan(gk/ak);
-        //return (r * 180.0) / M_PI;
-        return 1.0;
-    }
-    else if (a.y == b.y)
-    {
-        // calculate ak
-        if (a.x < b.x)
-        {
-            ak = b.x - a.x;
-        }
-        else
-        {
-            ak = a.x - b.x;
-        }
-
-        // calculate gk
-        if (a.z < b.z)
-        {
-            gk = b.z - a.z;
-        }
-        else
-        {
-            gk = a.z - b.z;
-        }
-
-        //calculate tan
-        r = atan(gk/ak);
-        ROS_INFO_STREAM("r: " << r << ", ak: " << ak << ", gk: " << gk);
-        ROS_INFO_STREAM("a.X: " << a.x << ", b.X: " << b.x);
-        ROS_INFO_STREAM("a.Y: " << a.y << ", b.Y: " << b.y);
-        ROS_INFO_STREAM("a.Z: " << a.z << ", b.Z: " << b.z);
-        return (r * 180.0) / M_PI;
-        //return 2.0;
-    }
-    else
-    {
-        //ROS_INFO_STREAM("a.X: " << a.x << ", b.X: " << b.x << ", a.Y: " << a.y << ", b.Y: " << b.y);
-        return 0.0;
-        
-    }
-}
-
+// Rotates the given pointcloud.
 void rectifyTilt(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
 {
     Eigen::Affine3f transform = Eigen::Affine3f::Identity();
@@ -348,48 +284,91 @@ void removeGroundPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
     pass_filter.filter (*input);
 }
 
-void findCylinder(pcl::PointCloud<pcl::PointXYZ>::Ptr& input)
+// This function uses RANSAC to fit a cylinder in to the given pointcloud.
+void findCylinder(pcl::PointCloud<pcl::PointXYZ>::Ptr& input, pcl::ModelCoefficients::Ptr& output_coefficients, pcl::PointIndices::Ptr& output_point_indicies)
 {
-    // Ransac
-    pcl::ModelCoefficients::Ptr coefficients_ptr (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-    ne.setInputCloud(input);
-
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
-    ne.setSearchMethod(tree);
-
     pcl::PointCloud<pcl::Normal>::Ptr cloud_normals_ptr (new pcl::PointCloud<pcl::Normal>);
-    
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+    pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> ne;
+    //Eigen::Vector3f axis = Eigen::Vector3f(1.0,0.0,0.0);
+    ne.setNumberOfThreads(8);
     ne.setKSearch(30);
-
+    ne.setInputCloud(input);
+    ne.setSearchMethod(tree);
     ne.compute(*cloud_normals_ptr);
 
     // create RandomSampleConsensus object and compute the appropriated model
-    pcl::SACSegmentationFromNormals<pcl::PointXYZ, pcl::Normal> seg;
+    pcl::SACSegmentationFromNormals<pcl::PointXYZ, pcl::Normal> seg (true);
+    seg.setOptimizeCoefficients(true);
     seg.setModelType(pcl::SACMODEL_CYLINDER);
     seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setNormalDistanceWeight (0.05);
-    seg.setMaxIterations (10000);
-    seg.setDistanceThreshold(0.4);
-    seg.setRadiusLimits(0.001, 0.1);
+    seg.setNormalDistanceWeight (0.2);
+    seg.setMaxIterations (50000);
+    seg.setDistanceThreshold(0.009);
+    seg.setRadiusLimits(0.001, 0.015);
+    //seg.setAxis(axis);
+    //seg.setEpsAngle(10.0f * (M_PI/180.0f));
     seg.setInputCloud(input);
-    seg.setInputNormals (cloud_normals_ptr);
+    seg.setInputNormals(cloud_normals_ptr);
     seg.setNumberOfThreads(8);
-    seg.segment(*inliers, *coefficients_ptr);
+    seg.segment(*output_point_indicies, *output_coefficients);
 
-    ROS_INFO_STREAM("Cylinder inliers: " << inliers->indices.size());
+    ROS_INFO_STREAM("Cylinder coefficients (x, y, z, ax, ay, az): ");
+    for (int i = 0; i < 7; i++)
+    {
+        ROS_INFO_STREAM(i+1 << " = " << output_coefficients->values.at(i));
+    }
+    
+    ROS_INFO_STREAM("Cylinder inliers: " << output_point_indicies->indices.size());
 
+    /*
     pcl::ExtractIndices<pcl::PointXYZ> extr_inliers_filter;
     extr_inliers_filter.setInputCloud(input);
     extr_inliers_filter.setIndices(inliers);
     extr_inliers_filter.setNegative(false);
     extr_inliers_filter.filter(*input);
+    */
 }
 
-// This function sets the marker position to the given point.
-void setMarker(visualization_msgs::MarkerPtr& input_marker, pcl::PointXYZ input_point)
+// This function uses RANSAC to fit a line in to the given pointcloud.
+void findLine(pcl::PointCloud<pcl::PointXYZ>::Ptr& input, pcl::ModelCoefficients::Ptr& output_coefficients)
+{
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    Eigen::Vector3f axis = Eigen::Vector3f(1.0,0.0,0.0);
+
+    // create RandomSampleConsensus object and compute the appropriated model
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_LINE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations (100000);
+    seg.setDistanceThreshold(0.001);
+    //seg.setRadiusLimits(0.002, 0.003);
+    //seg.setAxis(axis);
+    //seg.setEpsAngle(10.0f * (M_PI/180.0f));
+    seg.setInputCloud(input);
+    seg.setNumberOfThreads(8);
+    seg.segment(*inliers, *output_coefficients);
+
+    ROS_INFO_STREAM("Line coefficients (x, y, z, ax, ay, az): ");
+    for (int i = 0; i < 6; i++)
+    {
+        ROS_INFO_STREAM(i+1 << " = " << output_coefficients->values.at(i));
+    }
+    
+    //ROS_INFO_STREAM("Cylinder inliers: " << inliers->indices.size());
+
+    /*
+    pcl::ExtractIndices<pcl::PointXYZ> extr_inliers_filter;
+    extr_inliers_filter.setInputCloud(input);
+    extr_inliers_filter.setIndices(inliers);
+    extr_inliers_filter.setNegative(false);
+    extr_inliers_filter.filter(*input);
+    */
+}
+
+// This function sets a sphere marker to the given point.
+void setPointMarker(visualization_msgs::MarkerPtr& input_marker, pcl::PointXYZ input_point)
 {
     input_marker->header.frame_id = "depth_camera_link";
     input_marker->header.stamp = ros::Time::now();
@@ -421,6 +400,123 @@ void setMarker(visualization_msgs::MarkerPtr& input_marker, pcl::PointXYZ input_
     input_marker->lifetime = ros::Duration();
 }
 
+// This function sets a cylinder marker to the given coefficients.
+void setCylinderMarker(visualization_msgs::MarkerPtr& input_marker, pcl::ModelCoefficients& input_coefficients)
+{    
+    Eigen::Quaternionf quaternion_from_coefficients;
+
+    quaternion_from_coefficients = obtainCylinderOrientationFromModel(input_coefficients);
+
+    input_marker->header.frame_id = "depth_camera_link";
+    input_marker->header.stamp = ros::Time::now();
+    input_marker->type = visualization_msgs::Marker::CYLINDER;
+    input_marker->ns = "cylinder";
+    input_marker->id = 0;
+    input_marker->action = visualization_msgs::Marker::ADD;
+
+    // Set the position and orientation of the marker
+    input_marker->pose.position.x = input_coefficients.values.at(0);
+    input_marker->pose.position.y = input_coefficients.values.at(1);
+    input_marker->pose.position.z = input_coefficients.values.at(2);
+    input_marker->pose.orientation.x = quaternion_from_coefficients.x();
+    input_marker->pose.orientation.y = quaternion_from_coefficients.y();
+    input_marker->pose.orientation.z = quaternion_from_coefficients.z();
+    input_marker->pose.orientation.w = quaternion_from_coefficients.w();
+  
+    // Set the scale of the marker
+    input_marker->scale.x = input_coefficients.values.at(6)*2;
+    input_marker->scale.y = input_coefficients.values.at(6)*2;
+    input_marker->scale.z = 10.0;
+  
+    // Set the color of the marker
+    input_marker->color.r = 1.0f;
+    input_marker->color.g = 1.0f;
+    input_marker->color.b = 1.0f;
+    input_marker->color.a = 0.6;
+
+    input_marker->lifetime = ros::Duration();
+}
+
+// This function sets a line marker to the given coefficients.
+void setLineMarker(visualization_msgs::MarkerPtr& input_marker, pcl::ModelCoefficients& input_coefficients)
+{    
+    // Convert axis vector to quarternion format
+    double axis_roll = atan2(input_coefficients.values[5],input_coefficients.values[4]);
+    double axis_pitch = -1.0 * atan2(input_coefficients.values[5],input_coefficients.values[3]);
+    double axis_yaw = atan2(input_coefficients.values[4],input_coefficients.values[3]);
+
+    tf2::Quaternion axis_quarternion;
+    axis_quarternion.setRPY( 0.0, -0.5*M_PI + axis_pitch, axis_yaw);
+    //axis_quarternion.setRPY( axis_roll, axis_pitch, axis_yaw);
+    axis_quarternion.normalize();
+
+    input_marker->header.frame_id = "depth_camera_link";
+    input_marker->header.stamp = ros::Time::now();
+    input_marker->type = visualization_msgs::Marker::CYLINDER;
+    input_marker->ns = "cylinder";
+    input_marker->id = 0;
+    input_marker->action = visualization_msgs::Marker::ADD;
+
+    // Set the position and orientation of the marker
+    input_marker->pose.position.x = input_coefficients.values.at(0);
+    input_marker->pose.position.y = input_coefficients.values.at(1);
+    input_marker->pose.position.z = input_coefficients.values.at(2);
+    input_marker->pose.orientation.x = axis_quarternion.getX();
+    input_marker->pose.orientation.y = axis_quarternion.getY();
+    input_marker->pose.orientation.z = axis_quarternion.getZ();
+    input_marker->pose.orientation.w = axis_quarternion.getW();
+  
+    // Set the scale of the marker
+    input_marker->scale.x = 0.01;
+    input_marker->scale.y = 0.01;
+    input_marker->scale.z = 10.0;
+  
+    // Set the color of the marker
+    input_marker->color.r = 1.0f;
+    input_marker->color.g = 1.0f;
+    input_marker->color.b = 1.0f;
+    input_marker->color.a = 0.8;
+
+    input_marker->lifetime = ros::Duration();
+}
+
+// This function sets a points marker to the given point indices.
+void setPointListMarker(visualization_msgs::MarkerPtr& input_marker, pcl::PointIndicesPtr& input_point_indices, pcl::PointCloud<pcl::PointXYZ>::Ptr& input_pointcloud)
+{    
+    input_marker->header.frame_id = "depth_camera_link";
+    input_marker->header.stamp = ros::Time::now();
+    input_marker->type = visualization_msgs::Marker::POINTS;
+    input_marker->ns = "inliers";
+    input_marker->id = 0;
+    input_marker->action = visualization_msgs::Marker::ADD;
+
+    int input_point_indices_size = input_point_indices->indices.size();
+
+    for (int i = 0; i < input_point_indices->indices.size(); i++)
+    {
+        geometry_msgs::Point p;
+
+        p.x = input_pointcloud->at(input_point_indices->indices.at(i)).x;
+        p.y = input_pointcloud->at(input_point_indices->indices.at(i)).y;
+        p.z = input_pointcloud->at(input_point_indices->indices.at(i)).z;
+
+        input_marker->points.push_back(p);
+    }
+  
+    // Set the scale of the marker
+    input_marker->scale.x = 0.003;
+    input_marker->scale.y = 0.003;
+    input_marker->scale.z = 0.003;
+  
+    // Set the color of the marker
+    input_marker->color.r = 0.0f;
+    input_marker->color.g = 0.0f;
+    input_marker->color.b = 1.0f;
+    input_marker->color.a = 1.0;
+
+    input_marker->lifetime = ros::Duration();
+}
+
 void dpCallback(const sensor_msgs::PointCloud2& sen_msg_pc2)
 {
     /* // start timer
@@ -431,9 +527,18 @@ void dpCallback(const sensor_msgs::PointCloud2& sen_msg_pc2)
     // create pcl pointcloud and convert sensor_msgs pointcloud2 to pcl pointcloud
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_pc_ptr(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(sen_msg_pc2, *pcl_pc_ptr);
+    pcl::PointIndices::Ptr inliers_ptr (new pcl::PointIndices);
+
+    pcl::ModelCoefficients::Ptr cylinder_coefficients_ptr (new pcl::ModelCoefficients);
+    pcl::ModelCoefficients::Ptr line_coefficients_ptr (new pcl::ModelCoefficients);
 
     // create a marker and pointer
-    visualization_msgs::MarkerPtr marker_ptr(new visualization_msgs::Marker);
+    visualization_msgs::MarkerPtr point_marker_ptr(new visualization_msgs::Marker);
+    visualization_msgs::MarkerPtr cylinder_marker_ptr(new visualization_msgs::Marker);
+    visualization_msgs::MarkerPtr line_marker_ptr(new visualization_msgs::Marker);
+    visualization_msgs::MarkerPtr points_marker_ptr(new visualization_msgs::Marker);
+
+    ROS_INFO_STREAM("Seq. Nr.: " << pcl_pc_ptr->header.seq);
 
     // fix tilted pointcloud
     rectifyTilt(pcl_pc_ptr);
@@ -443,17 +548,29 @@ void dpCallback(const sensor_msgs::PointCloud2& sen_msg_pc2)
     // remove every point from the pointcloud representing the groundplane
     removeGroundPlane(pcl_pc_ptr);
 
+    //moveDown(pcl_pc_ptr);
+
     // mirror pointcloud
     //mirrorPointcloudZ(pcl_pc_ptr);
 
     // setup marker
-    setMarker(marker_ptr, getPointMaxZ(pcl_pc_ptr));
+    //setPointMarker(point_marker_ptr, getPointMaxZ(pcl_pc_ptr));
 
     // find cylinder
-    findCylinder(pcl_pc_ptr);
+    findCylinder(pcl_pc_ptr, cylinder_coefficients_ptr, inliers_ptr);
+
+    // find line
+    //findLine(pcl_pc_ptr,line_coefficients_ptr);
+
+    setPointListMarker(points_marker_ptr, inliers_ptr, pcl_pc_ptr);
+
+    setCylinderMarker(cylinder_marker_ptr, *cylinder_coefficients_ptr);
+    //setLineMarker(line_marker_ptr, *line_coefficients_ptr);
 
     // publish marker to "visualization_marker" topic
-    pub_marker.publish(marker_ptr);
+    pub_marker.publish(points_marker_ptr);
+    pub_marker.publish(cylinder_marker_ptr);
+    //pub_marker.publish(line_marker_ptr);
 
     // publish pointcloud to "pclEdit" topic
     pub_pointcloud.publish(*pcl_pc_ptr);
